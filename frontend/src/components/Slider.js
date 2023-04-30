cf1f0eede66b3977d0d36d3ebd9bf5d08869f71c
Oscar <david.rubiano1702@gmail.com>
2023-04-29 19:13:54 -0500

    Login 1.0

---
+++ frontend/src/components/Slider.js
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './index.css';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
+import slide_image_1 from './images/Girasoles.jpg';
+import slide_image_2 from './images/LaNocheEstrellada.jpg';
+import slide_image_3 from './images/MonaLisa.jpg';
+
+function Slider() {
+  return (
+    <div className="container">
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slider;
